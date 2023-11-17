108c04a9483c946e9fa838aade5e929ba46afc71
EstebanC <estebancogo47@gmail.com>
2023-11-17 17:19:08 -0500

    Empezando desarrollo del botón limpiar 🏁

---
+++ WebSocket-Front/src/components/BotonLimpiar.jsx
@@ -0,0 +1,17 @@
+// BotonLimpia.jsx
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BotonLimpia = ({ onLimpiarTablero }) => {
+  return (
+    <div>
+      <button onClick={onLimpiarTablero}>Limpiar Tablero</button>
+    </div>
+  );
+};
+
+BotonLimpia.propTypes = {
+  onLimpiarTablero: PropTypes.func.isRequired,
+};
+
+export default BotonLimpia;
