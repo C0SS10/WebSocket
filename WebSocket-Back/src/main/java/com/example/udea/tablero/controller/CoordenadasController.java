1c6ab5c53efbcc80744cdf855eb0bc5a21bcf53b
Cossio <estebancogo47@gmail.com>
2023-11-20 12:46:49 -0500

    Ahora se puede hacer uso del websocket ☑️

--- WebSocket-Back/src/main/java/com/example/udea/tablero/controller/CoordenadasController.java
+++ WebSocket-Back/src/main/java/com/example/udea/tablero/controller/CoordenadasController.java
@@ -3,16 +3,14 @@ package com.example.udea.tablero.controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.udea.tablero.model.Coordenadas;
 
 @Controller
-@CrossOrigin(origins = "*")
 public class CoordenadasController {
 
     @MessageMapping("/tablero")
-    @SendTo("/tablero/coordenadas")
+    @SendTo("/tablero/coordenada")
     public Coordenadas enviarCoordenadas(Coordenadas coordenadas) {
         return coordenadas;
     }
